a35af76db8378341e755f1ccedbd84dec2e5d80e
Renan Bueno <renanbuenov@outlook.com>
2022-08-12 19:40:08 -0300

    Created 2 more listeners.

--- str-producer/str-producer/src/main/java/com/renan/strproducer/services/StringProducerService.java
+++ str-producer/str-producer/src/main/java/com/renan/strproducer/services/StringProducerService.java
@@ -14,16 +14,18 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message).addCallback(
-                success -> {
-                    if(success != null) {
-                        log.info("Send message successful {}", message);
-                        log.info("Partition {}, Offset {}",
-                                success.getRecordMetadata().partition(),
-                                success.getRecordMetadata().offset());
-                    }
-                },
-                error -> log.info("Error sending message")
-        );
+        log.info("Send message {}", message);
+            kafkaTemplate.send("str-topic", message);
+//                    .addCallback(
+//                    success -> {
+//                        if(success != null) {
+//                            log.info("Send message successful {}", message);
+//                            log.info("Partition {}, Offset {}",
+//                                    success.getRecordMetadata().partition(),
+//                                    success.getRecordMetadata().offset());
+//                        }
+//                    },
+//                    error -> log.info("Error sending message")
+//            );
     }
 }
