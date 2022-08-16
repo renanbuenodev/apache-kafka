0cf6a348f3b8255da087e563f6d25aeca3d4e056
Renan Bueno <renanbuenov@outlook.com>
2022-08-16 11:13:56 -0300

    Error handling

---
+++ str-consumer/src/main/java/com/renan/strconsumer/exceptions/ErrorCustomHandler.java
@@ -0,0 +1,18 @@
+package com.renan.strconsumer.exceptions;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class ErrorCustomHandler implements KafkaListenerErrorHandler {
+
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
+        log.info("EXCEPTION_HANDLER ::: Capturei um erro");
+        return null;
+    }
+}
