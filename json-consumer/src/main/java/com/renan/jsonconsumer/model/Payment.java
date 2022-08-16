96b7cb6d81f53abc8c24e53598e582ade4b6223b
Renan Bueno <renanbuenov@outlook.com>
2022-08-16 12:58:35 -0300

    Add json consumer

---
+++ json-consumer/src/main/java/com/renan/jsonconsumer/model/Payment.java
@@ -0,0 +1,19 @@
+package com.renan.jsonconsumer.model;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@ToString
+@Getter
+public class Payment implements Serializable {
+
+    private Long id;
+    private Long idUser;
+    private Long idProduct;
+    private String cardNumber;
+
+
+
+}
